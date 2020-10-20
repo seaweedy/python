156b7dccbbe176aedbcdd82551f933340071b7fe
deewaes <ismh5279@gmail.com>
2020-10-20 17:10:18 +0900

    django를 이용한 html연결

--- Mysite/Mysite/views.py
+++ Mysite/Mysite/views.py
@@ -1,6 +1,9 @@
  
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-  return HttpResponse("I'm Headache")
+  return HttpResponse("index page")
+
+def index2(request):
+  return render(request,'headache.html')
